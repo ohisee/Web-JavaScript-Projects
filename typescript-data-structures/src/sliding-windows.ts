a3a19861f4b9e598fe78188edcce853dea6f5f98
ohisee <breakosin@hotmail.com>
2020-02-24 00:20:43 -0800

    Data structures

--- typescript-data-structures/src/sliding-windows.ts
+++ typescript-data-structures/src/sliding-windows.ts
@@ -21,3 +21,52 @@ export function maxSubarraySum(arr: number[], n: number): number | null {
 }
 
 // console.log(maxSubarraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3));
+
+/**
+ * Sliding window
+ * @param arr 
+ * @param windowSize 
+ */
+export function maxSubarraySumUsingSlidingWindow(arr: number[], windowSize: number): number | null {
+  if (arr.length < windowSize) {
+    return null;
+  }
+  let maxSum = 0;
+  let tempSum = 0;
+  for (let i = 0; i < windowSize; i++) {
+    tempSum += arr[i];
+  }
+  maxSum = tempSum;
+  for (let j = windowSize; j < arr.length; j++) {
+    tempSum = tempSum + arr[j] - arr[j - windowSize];
+    maxSum = Math.max(tempSum, maxSum);
+  }
+  return maxSum;
+}
+
+export function minSubarrayLen(arr: number[], target: number): number {
+  if (arr.length === 0) {
+    return 0;
+  }
+  let windowSize = 0;
+  let minLen = arr.length;
+  let sum = 0;
+  let indexCounter = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    if (sum >= target) {
+      windowSize = (i + 1) - indexCounter;
+      // console.log(i, arr[i], sum, minLen, windowSize);
+      let counter = 0;
+      while (sum >= target) {
+        // console.log(arr.slice(indexCounter, i + 1));
+        sum -= arr[indexCounter];
+        indexCounter++;
+        counter++;
+      }
+      windowSize = windowSize - (counter - 1);
+      minLen = Math.min(minLen, windowSize);
+    }
+  }
+  return Math.min(minLen, windowSize);
+}
--- typescript-data-structures/src/tests/sliding-windows.spec.ts
+++ typescript-data-structures/src/tests/sliding-windows.spec.ts
@@ -3,12 +3,69 @@
  */
 import { expect } from "chai";
 import "mocha";
-import { maxSubarraySum } from "../sliding-windows";
+import {
+  maxSubarraySum,
+  maxSubarraySumUsingSlidingWindow,
+  minSubarrayLen
+} from "../sliding-windows";
 
 describe('Sliding window', () => {
+  describe('Max sub array', () => {
+    it('Should return 19', () => {
+      expect(maxSubarraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3)).equals(19);
+    });
+  });
 
-  it('', () => {
-    expect(maxSubarraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3)).equals(19);
+  describe('Max sub array sum using sliding window', () => {
+    it('Should return null', () => {
+      expect(maxSubarraySumUsingSlidingWindow([1, 2, 10, 9, 6], 8)).equals(null);
+    });
+
+    it('Should return 700', () => {
+      expect(maxSubarraySumUsingSlidingWindow([100, 200, 300, 400, 100, 90], 2)).equals(700);
+    });
+
+    it('Should return 39', () => {
+      expect(maxSubarraySumUsingSlidingWindow([1, 4, 2, 10, 23, 3, 1, 0, 20], 4)).equals(39);
+    });
+
+    it('(1) Should return 5', () => {
+      expect(maxSubarraySumUsingSlidingWindow([-3, 4, 0, -2, 6, -1], 2)).equals(5);
+    });
+
+    it('(2) Should return 5', () => {
+      expect(maxSubarraySumUsingSlidingWindow([3, -2, 7, -4, 1, -1, 4, -2, 1], 2)).equals(5);
+    });
+  });
+
+  describe('Min sub array length using sliding window', () => {
+    it('(1) Should return 2', () => {
+      expect(minSubarrayLen([2, 3, 1, 2, 4, 3], 7)).equals(2);
+    });
+
+    it('(2) Should return 2', () => {
+      expect(minSubarrayLen([2, 1, 6, 5, 4], 9)).equals(2);
+    });
+
+    it('Should return 1', () => {
+      expect(minSubarrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52)).equals(1);
+    });
+
+    it('Should return 3', () => {
+      expect(minSubarrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 39)).equals(3);
+    });
+
+    it('Should return 5', () => {
+      expect(minSubarrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 55)).equals(5);
+    });
+
+    it('Should return 2', () => {
+      expect(minSubarrayLen([4, 3, 3, 8, 1, 2, 3], 11)).equals(2);
+    });
+
+    it('Should return 0', () => {
+      expect(minSubarrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 95)).equals(0);
+    });
   });
 
 });
