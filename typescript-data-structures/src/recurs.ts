55e635c6fcd5b43b104ce46b54a78fcf23b34a6c
ohisee <breakosin@hotmail.com>
2020-03-02 23:52:37 -0800

    Data structures

--- typescript-data-structures/src/recurs.ts
+++ typescript-data-structures/src/recurs.ts
@@ -10,3 +10,72 @@ export function power(base: number, exponent: number): number {
   }
   return base * power(base, exponent - 1);
 }
+
+export function factorial(num: number): number {
+  if (num < 0) {
+    return 0;
+  }
+  if (num <= 1) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+}
+
+export function productOfArray(arr: number[]): number {
+  if (arr.length === 0) {
+    return 0;
+  }
+  if (arr.length === 1) {
+    return arr[0];
+  }
+  return arr[0] * productOfArray(arr.slice(1));
+}
+
+export function recursiveRange(num: number): number {
+  if (num === 0) {
+    return 0;
+  }
+  return num + recursiveRange(num - 1);
+}
+
+export function fib(num: number): number {
+  if (num === 0) {
+    return 0;
+  }
+  if (num === 1 || num === 2) {
+    return 1;
+  }
+  return fib(num - 1) + fib(num - 2);
+}
+
+export function reverse(str: string): string {
+  if (str.length === 1) {
+    return str[0];
+  }
+  return str[str.length - 1] + reverse(str.substring(0, str.length - 1));
+}
+
+export function isPalindrome(str: string): boolean {
+  if (str.length <= 3) {
+    return str[0] === str[str.length - 1];
+  }
+  return str[0] === str[str.length - 1] && isPalindrome(str.substring(1, str.length - 1));
+}
+
+export function someRecursive(arr: number[], callback: (arg: number) => boolean): boolean {
+  if (arr.length === 1) {
+    return callback(arr[0]);
+  }
+  return callback(arr[0]) || someRecursive(arr.slice(1), callback);
+}
+
+export function flatten(arr: any): any[] {
+  if (!Array.isArray(arr)) {
+    return [arr];
+  }
+  let result: any[] = [];
+  for (let i of arr) {
+    result = result.concat(flatten(i));
+  }
+  return result;
+}
--- typescript-data-structures/src/tests/recurs.spec.ts
+++ typescript-data-structures/src/tests/recurs.spec.ts
@@ -3,16 +3,155 @@
  */
 import { expect } from "chai";
 import "mocha";
-import { power } from "../recurs";
+import {
+  power,
+  factorial,
+  productOfArray,
+  recursiveRange,
+  fib,
+  reverse,
+  isPalindrome,
+  someRecursive,
+  flatten
+} from "../recurs";
 
 describe('Recursion', () => {
+
   describe('Power using recursion', () => {
     it('Should return 1', () => {
-      expect(power(2, 0)).equals(1);
+      expect(power(3, 0)).equals(1);
     });
 
-    it('Should return 8', () => {
-      expect(power(2, 3)).equals(8);
+    it('Should return 27', () => {
+      expect(power(3, 3)).equals(27);
+    });
+
+    it('Should return 1024', () => {
+      expect(power(2, 10)).equals(1024);
+    });
+  });
+
+  describe('Factorial using recursion', () => {
+    it('Should return 1', () => {
+      expect(factorial(0)).equals(1);
+    });
+
+    it('Should return 1', () => {
+      expect(factorial(1)).equals(1);
+    });
+
+    it('Should return 6', () => {
+      expect(factorial(3)).equals(6);
+    });
+
+    it('Should return 5040', () => {
+      expect(factorial(7)).equals(5040);
+    });
+  });
+
+  describe('Product of array using recursion', () => {
+    it('Should return 6', () => {
+      expect(productOfArray([1, 2, 3])).equals(6);
+    });
+
+    it('Should return 60', () => {
+      expect(productOfArray([1, 2, 3, 10])).equals(60);
+    });
+
+    it('Should return 27', () => {
+      expect(productOfArray([3, 3, 3])).equals(27);
+    });
+  });
+
+  describe('Recursive range using recursion', () => {
+    it('Should return 21', () => {
+      expect(recursiveRange(6)).equals(21);
+    });
+
+    it('Should return 55', () => {
+      expect(recursiveRange(10)).equals(55);
+    });
+  });
+
+  describe('Fibonacci using recursion', () => {
+    it('Should return 3', () => {
+      expect(fib(4)).equals(3);
+    });
+
+    it('Should return 55', () => {
+      expect(fib(10)).equals(55);
+    });
+
+    it('Should return 317811', () => {
+      expect(fib(28)).equals(317811);
+    });
+
+    it('Should return 9227465', () => {
+      expect(fib(35)).equals(9227465);
+    });
+  });
+
+  describe('Reverse using recursion', () => {
+    it('Should return emosewa', () => {
+      expect(reverse('awesome')).equals('emosewa');
+    });
+
+    it('Should return loohcsmhtir', () => {
+      expect(reverse('rithmschool')).equals('loohcsmhtir');
+    });
+
+    it('Should return eeeeeeeb', () => {
+      expect(reverse('beeeeeee')).equals('eeeeeeeb');
+    });
+  });
+
+  describe('Check is palindrome using recursion', () => {
+    it('Should return eee', () => {
+      expect(isPalindrome('eee')).equals(true);
+    });
+
+    it('Should return false', () => {
+      expect(isPalindrome('awesome')).equals(false);
+    });
+
+    it('Should return false', () => {
+      expect(isPalindrome('foobar')).equals(false);
+    });
+
+    it('Should return true', () => {
+      expect(isPalindrome('tacocat')).equals(true);
+    });
+
+    it('Should return true', () => {
+      expect(isPalindrome('amanaplanacanalpanama')).equals(true);
+    });
+
+    it('Should return false', () => {
+      expect(isPalindrome('amanaplanacanalpandemonium')).equals(false);
+    });
+  });
+
+  describe('Some recursive with callback', () => {
+    it('Should return true', () => {
+      expect(someRecursive([1, 2, 3, 4], val => val % 2 !== 0)).equals(true);
+    });
+
+    it('Should return true', () => {
+      expect(someRecursive([4, 6, 8, 9], val => val % 2 !== 0)).equals(true);
+    });
+
+    it('Should return false', () => {
+      expect(someRecursive([4, 6, 8], val => val % 2 !== 0)).equals(false);
+    });
+
+    it('Should return false', () => {
+      expect(someRecursive([4, 6, 8], val => val > 10)).equals(false);
+    });
+  });
+
+  describe('Flatten array using recursion', () => {
+    it('Should return [1,2,3,4,5]', () => {
+      expect(flatten([1, [2], 3, [4, 5]])).to.eql([1, 2, 3, 4, 5]);
     });
   });
 });
