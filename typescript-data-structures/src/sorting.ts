ce9913e37d4928d98ce5c04d1410bf5ac0451263
ohisee <breakosin@hotmail.com>
2020-03-12 23:38:32 -0700

    Data structures

--- typescript-data-structures/src/sorting.ts
+++ typescript-data-structures/src/sorting.ts
@@ -25,10 +25,8 @@ export function bubbleSort(arr: number[]): number[] {
 export function selectionSort(arr: number[]): number[] {
   for (let i = 0; i < arr.length; i++) {
     let minLoc = i;
-    let min = arr[i];
     for (let j = i + 1; j < arr.length; j++) {
-      if (arr[j] < min) {
-        min = arr[j];
+      if (arr[j] < arr[minLoc]) {
         minLoc = j;
       }
     }
@@ -39,4 +37,62 @@ export function selectionSort(arr: number[]): number[] {
     }
   }
   return arr;
+}
+
+export function insertionSortUsingSwap(arr: number[]): number[] {
+  for (let i = 1; i < arr.length; i++) {
+    // insert from right to left of the array
+    for (let j = i; j > 0; j--) {
+      if (arr[j] < arr[j - 1]) {
+        let t = arr[j - 1];
+        arr[j - 1] = arr[j];
+        arr[j] = t;
+      }
+    }
+  }
+  return arr;
+}
+
+export function insertionSort(arr: number[]): number[] {
+  for (let i = 1; i < arr.length; i++) {
+    let currentVal = arr[i];
+    // set greater than current value in jth position
+    let j = i;
+    while (j > 0 && arr[j - 1] > currentVal) {
+      arr[j] = arr[j - 1];
+      j--;
+    }
+    arr[j] = currentVal;
+  }
+  return arr;
+}
+
+export function merge(arr1: number[], arr2: number[]): number[] {
+  let result: number[] = [];
+  let i = 0;
+  let j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      result.push(arr1[i]);
+      i++;
+    } else {
+      result.push(arr2[j]);
+      j++;
+    }
+  }
+  // if (i === arr1.length && j < arr2.length) {
+  //   result.push(...arr2.slice(j));
+  // }
+  // if (j === arr2.length && i < arr1.length) {
+  //   result.push(...arr1.slice(i));
+  // }
+  while (i < arr1.length) {
+    result.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    result.push(arr2[j]);
+    j++;
+  }
+  return result;
 }
--- typescript-data-structures/src/tests/sorting.spec.ts
+++ typescript-data-structures/src/tests/sorting.spec.ts
@@ -3,7 +3,13 @@
  */
 import { expect } from "chai";
 import "mocha";
-import { bubbleSort, selectionSort } from "../sorting";
+import {
+  bubbleSort,
+  selectionSort,
+  insertionSortUsingSwap,
+  insertionSort,
+  merge
+} from "../sorting";
 
 describe('Sort', () => {
   describe('Bubble sort', () => {
@@ -31,6 +37,48 @@ describe('Sort', () => {
 
     it('Should return [1, 2, 3, 4, 5, 6, 7, 8]', () => {
       expect(selectionSort([8, 1, 2, 3, 4, 5, 6, 7])).to.eql([1, 2, 3, 4, 5, 6, 7, 8]);
+    });
+  });
+
+  describe('Insertion sort', () => {
+    it('Should return [2, 3, 9, 10, 11, 25]', () => {
+      expect(insertionSortUsingSwap([10, 2, 15, 9, 3, 11])).to.eql([2, 3, 9, 10, 11, 15]);
+    });
+
+    it('Should return [2, 9, 10, 11, 15, 30]', () => {
+      expect(insertionSortUsingSwap([10, 2, 15, 9, 30, 11])).to.eql([2, 9, 10, 11, 15, 30]);
+    });
+
+    it('Should return [1, 2, 3, 4, 5, 6, 7, 8]', () => {
+      expect(insertionSortUsingSwap([8, 1, 2, 3, 4, 5, 6, 7])).to.eql([1, 2, 3, 4, 5, 6, 7, 8]);
+    });
+  });
+
+  describe('Insertion sort', () => {
+    it('Should return [2, 3, 9, 10, 11, 25]', () => {
+      expect(insertionSort([10, 2, 15, 9, 3, 11])).to.eql([2, 3, 9, 10, 11, 15]);
+    });
+
+    it('Should return [2, 9, 10, 11, 15, 30]', () => {
+      expect(insertionSort([10, 2, 15, 9, 30, 11])).to.eql([2, 9, 10, 11, 15, 30]);
+    });
+
+    it('Should return [1, 2, 3, 4, 5, 6, 7, 8]', () => {
+      expect(insertionSort([8, 1, 2, 3, 4, 5, 6, 7])).to.eql([1, 2, 3, 4, 5, 6, 7, 8]);
+    });
+  });
+
+  describe('Merge two arrays', () => {
+    it('Should return [2, 5, 6, 7, 8, 9, 10, 11]', () => {
+      expect(merge([2, 8, 9, 10, 11], [5, 6, 7])).to.eql([2, 5, 6, 7, 8, 9, 10, 11]);
+    });
+
+    it('Should return [2, 5, 6, 7, 8, 9, 10, 11]', () => {
+      expect(merge([5, 6, 7], [2, 8, 9, 10, 11])).to.eql([2, 5, 6, 7, 8, 9, 10, 11]);
+    });
+
+    it('Should return [[2, 5, 6, 7, 8, 9, 10, 11, 12, 18]', () => {
+      expect(merge([5, 6, 7, 12, 18], [2, 8, 9, 10, 11])).to.eql([2, 5, 6, 7, 8, 9, 10, 11, 12, 18]);
     });
   });
 });
