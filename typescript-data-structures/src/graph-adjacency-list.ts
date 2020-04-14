4749f230da09b8f77a1a96043ab8ef12282111a9
ohisee <breakosin@hotmail.com>
2020-04-14 11:56:13 -0700

    Data structures

---
+++ typescript-data-structures/src/graph-adjacency-list.ts
@@ -0,0 +1,27 @@
+/**
+ * @fileoverview undirected graph using adjaceny list
+ */
+
+export class Graph {
+
+  private adjacencyList: { [key: string]: any[] };
+
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex: string) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+    }
+  }
+
+  addEdge(vertex1: string, vertex2: string) {
+    if (this.adjacencyList[vertex1]) {
+      this.adjacencyList[vertex1].push(vertex2);
+    }
+    if (this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex2].push(vertex1);
+    }
+  }
+}
