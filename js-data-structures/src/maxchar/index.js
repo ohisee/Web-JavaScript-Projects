9d8d59a51d58d090237c358c19d8c49614fd4270
ohisee <breakosin@hotmail.com>
2020-05-20 22:24:59 -0700

    Data structures continued
    Added Jest to do unit testing

--- js-data-structures/src/maxchar/index.js
+++ js-data-structures/src/maxchar/index.js
@@ -4,8 +4,22 @@
  * maxChar("apple 1231111") === "1"
  */
 
-function maxChar() {
-
+function maxChar(str) {
+  const counter = {};
+  let maxCount = 0;
+  let result;
+  for (let character of str) {
+    if (counter[character]) {
+      counter[character] += 1;
+    } else {
+      counter[character] = 1;
+    }
+    if (counter[character] > maxCount) {
+      maxCount = counter[character];
+      result = character;
+    }
+  }
+  return result;
 }
 
 module.exports = {
--- js-data-structures/src/maxchar/maxchar.test.js
+++ js-data-structures/src/maxchar/maxchar.test.js
@@ -8,4 +8,12 @@ describe('Max char test', () => {
   test('Should return max char', () => {
     expect(maxChar('112')).toEqual('1');
   });
+
+  test('Should return max char', () => {
+    expect(maxChar('abcccccccd')).toEqual('c');
+  });
+
+  test('Should return max char', () => {
+    expect(maxChar('ab1c1d1e1f1g1')).toEqual('1');
+  })
 });
