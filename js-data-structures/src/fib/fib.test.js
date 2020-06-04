54e5cfc8ba5c921643b9081307b5702ded3b19e2
ohisee <breakosin@hotmail.com>
2020-06-04 16:45:07 -0700

    Data structures continued

--- js-data-structures/src/fib/fib.test.js
+++ js-data-structures/src/fib/fib.test.js
@@ -1,3 +1,83 @@
 /**
  * @fileoverview fib unit test
  */
+
+const { fib, fibRecursion, fibUsingMap } = require('./index');
+
+describe("Fibonaccis series", () => {
+  test('Function is defined', () => {
+    expect(fib).toBeDefined();
+  });
+
+  test('Should return fib series', () => {
+    expect(fib(1)).toEqual(1);
+  });
+
+  test('Should return fib series', () => {
+    expect(fib(2)).toEqual(1);
+  });
+
+  test('Should return fib series', () => {
+    expect(fib(3)).toEqual(2);
+  });
+
+  test('Should return fib series', () => {
+    expect(fib(9)).toEqual(34);
+  });
+
+  test('Should return fib series', () => {
+    expect(fib(12)).toEqual(144);
+  });
+
+  test('Should return fib series, check run time', () => {
+    expect(fib(38)).toEqual(39088169);
+  });
+
+  test('Should return fib series', () => {
+    expect(fibRecursion(1)).toEqual(1);
+  });
+
+  test('Should return fib series', () => {
+    expect(fibRecursion(2)).toEqual(1);
+  });
+
+  test('Should return fib series', () => {
+    expect(fibRecursion(3)).toEqual(2);
+  });
+
+  test('Should return fib series', () => {
+    expect(fibRecursion(9)).toEqual(34);
+  });
+
+  test('Should return fib series', () => {
+    expect(fibRecursion(12)).toEqual(144);
+  });
+
+  test('Should return fib series, check run time', () => {
+    expect(fibRecursion(38)).toEqual(39088169);
+  });
+
+  test('Should return fib series', () => {
+    expect(fibUsingMap(1)).toEqual(1);
+  });
+
+  test('Should return fib series', () => {
+    expect(fibUsingMap(2)).toEqual(1);
+  });
+
+  test('Should return fib series', () => {
+    expect(fibUsingMap(3)).toEqual(2);
+  });
+
+  test('Should return fib series', () => {
+    expect(fibUsingMap(9)).toEqual(34);
+  });
+
+  test('Should return fib series', () => {
+    expect(fibUsingMap(12)).toEqual(144);
+  });
+
+  test('Should return fib series, check run time', () => {
+    expect(fibUsingMap(38)).toEqual(39088169);
+  });
+});
--- js-data-structures/src/fib/index.js
+++ js-data-structures/src/fib/index.js
@@ -5,17 +5,40 @@
  * fibonaccis series of 10 is [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
  */
 
- /**
-  * O(n) run time
-  */
+/**
+ * O(n) run time
+ */
 function fib(num) {
   const result = [0, 1];
   for (let i = 2; i <= num; i++) {
     result.push(result[i - 1] + result[i - 2]);
   }
-  return result[num];
+  return result[result.length - 1];
+}
+
+function fibRecursion(num) {
+  if (num === 0) {
+    return 0;
+  }
+  if (num === 1) {
+    return 1;
+  }
+  return fibRecursion(num - 2) + fibRecursion(num - 1);
+}
+
+function fibUsingMap(num) {
+  const memo = { 0: 0, 1: 1 };
+  if (num === 0 || num === 1) {
+    return num;
+  }
+  for (let i = 2; i <= num; i++) {
+    memo[i] = memo[i - 2] + memo[i - 1];
+  }
+  return memo[num];
 }
 
 module.exports = {
-  fib
+  fib,
+  fibRecursion,
+  fibUsingMap
 };
