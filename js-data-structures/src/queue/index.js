747027eafa770b80422a6f88d4b95f0181ffbde8
ohisee <breakosin@hotmail.com>
2020-06-06 23:44:27 -0700

    Data structures continued

---
+++ js-data-structures/src/queue/index.js
@@ -0,0 +1,21 @@
+/**
+ * @fileoverview queue using array
+ */
+
+class Queue {
+  constructor() {
+    this.data = [];
+  }
+
+  add(item) {
+    this.data.unshift(item);
+  }
+
+  remove() {
+    return this.data.pop();
+  }
+}
+
+module.exports = {
+  Queue
+};
---
+++ js-data-structures/src/queue/queue.test.js
@@ -0,0 +1,17 @@
+/**
+ * @fileoverview queue unit test
+ */
+
+const { Queue } = require('./index');
+
+describe('Queue, first in first out', () => {
+  test('Queue class constructor is defined', () => {
+    expect(Queue.prototype.constructor).toBeDefined();
+  });
+
+  test('Should add and remove', () => {
+    const queue = new Queue();
+    expect(() => { queue.add(1); }).not.toThrow();
+    expect(queue.remove()).toEqual(1);
+  });
+});
