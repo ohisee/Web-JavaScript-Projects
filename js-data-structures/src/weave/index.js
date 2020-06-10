38b71b817f0ec4b4c98ee34fcb2911043b38d2f8
ohisee <breakosin@hotmail.com>
2020-06-10 00:17:45 -0700

    Data structures continued

--- js-data-structures/src/weave/index.js
+++ js-data-structures/src/weave/index.js
@@ -1,27 +1,24 @@
 /**
  * @fileoverview weave receives two queues as arguments and combines the 
- * contents of each into a new, third queue
- * the third queue should contain the *alterating* content of the two queues, 
- * should handle queues of different lengths without inserting 
- * 'undefined' into the new one.
+ * contents of each into a new queue
+ * the third queue should contain the alterating content of the two queues, 
+ * should handle queues of different lengths without inserting 'undefined' into the new one.
  */
 const { Queue } = require('./queue');
 
-function weave(sourceOne = new Queue(), sourceTwo = new Queue()) {
+/**
+ * @param {Queue} sourceOne 
+ * @param {Queue} sourceTwo 
+ */
+function weave(sourceOne, sourceTwo) {
   const result = new Queue();
-  while (sourceOne.peek() && sourceTwo.peek()) {
-    let sone = sourceOne.remove();
-    let stwo = sourceTwo.remove();
-    result.add(sone);
-    result.add(stwo);
-  }
-  while (sourceOne.peek()) {
-    let sone = sourceOne.remove();
-    result.add(sone);
-  }
-  while (sourceTwo.peek()) {
-    let stwo = sourceTwo.remove();
-    result.add(stwo);
+  while (sourceOne.peek() || sourceTwo.peek()) {
+    if (sourceOne.peek()) {
+      result.add(sourceOne.remove());
+    }
+    if (sourceTwo.peek()) {
+      result.add(sourceTwo.remove());
+    }
   }
   return result;
 };
--- js-data-structures/src/weave/weave.test.js
+++ js-data-structures/src/weave/weave.test.js
@@ -2,9 +2,92 @@
  * @fileoverview weave unit test
  */
 const { weave } = require('./index');
+const { Queue } = require('./queue');
 
 describe('Weave two queues', () => {
   test('Should define function', () => {
     expect(weave).toBeDefined();
+  });
+
+  test('Should define function', () => {
+    expect(Queue.prototype.peek).toBeDefined();
+  });
+
+  test('Should peek queue', () => {
+    const queue = new Queue();
+    queue.add(1);
+    queue.add(2);
+    expect(queue.peek()).toEqual(1);
+    expect(queue.peek()).toEqual(1);
+    expect(queue.remove()).toEqual(1);
+    expect(queue.remove()).toEqual(2);
+  });
+
+  test('Should weave queues', () => {
+    const queueOne = new Queue();
+    queueOne.add(1);
+    queueOne.add(2);
+    queueOne.add(3);
+    queueOne.add(4);
+    queueOne.add(5);
+    const queueTwo = new Queue();
+    queueTwo.add('one');
+    queueTwo.add('two');
+    queueTwo.add('three');
+    queueTwo.add('four');
+    queueTwo.add('five');
+
+    const result = weave(queueOne, queueTwo);
+    expect(result.remove()).toEqual(1);
+    expect(result.remove()).toEqual('one');
+    expect(result.remove()).toEqual(2);
+    expect(result.remove()).toEqual('two');
+    expect(result.remove()).toEqual(3);
+    expect(result.remove()).toEqual('three');
+    expect(result.remove()).toEqual(4);
+    expect(result.remove()).toEqual('four');
+    expect(result.remove()).toEqual(5);
+    expect(result.remove()).toEqual('five');
+    expect(result.remove()).toBeUndefined();
+  });
+
+  test('Should weave queues', () => {
+    const queueOne = new Queue();
+    queueOne.add(1);
+    queueOne.add(2);
+    const queueTwo = new Queue();
+    queueTwo.add('one');
+    queueTwo.add('two');
+    queueTwo.add('three');
+
+    const result = weave(queueOne, queueTwo);
+    expect(result.remove()).toEqual(1);
+    expect(result.remove()).toEqual('one');
+    expect(result.remove()).toEqual(2);
+    expect(result.remove()).toEqual('two');
+    expect(result.remove()).toEqual('three');
+    expect(result.remove()).toBeUndefined();
+  });
+
+  test('Should weave queues', () => {
+    const queueOne = new Queue();
+    queueOne.add(1);
+    queueOne.add(2);
+    queueOne.add(3);
+    queueOne.add(4);
+    queueOne.add(5);
+    const queueTwo = new Queue();
+    queueTwo.add('one');
+    queueTwo.add('two');
+
+    const result = weave(queueOne, queueTwo);
+    expect(result.remove()).toEqual(1);
+    expect(result.remove()).toEqual('one');
+    expect(result.remove()).toEqual(2);
+    expect(result.remove()).toEqual('two');
+    expect(result.remove()).toEqual(3);
+    expect(result.remove()).toEqual(4);
+    expect(result.remove()).toEqual(5);
+    expect(result.remove()).toBeUndefined();
   });
 });
