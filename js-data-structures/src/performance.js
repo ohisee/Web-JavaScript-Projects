3ea7a3ecb107d983c2d5d6bc9a9f96f160a6ec37
ohisee <breakosin@hotmail.com>
2020-07-13 14:24:14 -0700

    Data structures continued
    Performance check using Node.js perf_hooks

--- js-data-structures/src/performance.js
+++ js-data-structures/src/performance.js
@@ -4,18 +4,34 @@
  */
 const { performance } = require("perf_hooks");
 const { bubblesort } = require("./sorting/bubblesort");
+const { selectionSort } = require("./sorting/selectionsort");
+const { mergeSort } = require("./sorting/mergesort");
 
 const RECORDS = 10000;
+const MERGE_RECORDS = 20000;
 
-function gen() {
+/**
+ * @param {number} limit 
+ */
+function gen(limit) {
   const result = [];
-  for (let i = 0; i < RECORDS; i++) {
+  for (let i = 0; i < limit; i++) {
     result.push(Math.floor(Math.random() * 100) + (-100));
   }
   return result;
 }
 
 let t1 = performance.now();
-bubblesort(gen());
+bubblesort(gen(RECORDS));
 let t2 = performance.now();
-console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds`);
+console.log(`Time Elapsed after sorting ${RECORDS} elements: ${(t2 - t1) / 1000} seconds`);
+
+let t11 = performance.now();
+selectionSort(gen(RECORDS));
+let t22 = performance.now();
+console.log(`Time Elapsed after sorting ${RECORDS} elements: ${(t22 - t11) / 1000} seconds`);
+
+let t111 = performance.now();
+mergeSort(gen(MERGE_RECORDS));
+let t222 = performance.now();
+console.log(`Time Elapsed after sorting ${MERGE_RECORDS} elements: ${(t222 - t111) / 1000} seconds`);
