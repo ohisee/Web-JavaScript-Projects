cf2079e07e2b51e34b275589d72c82eef94b8379
ohisee <breakosin@hotmail.com>
2020-08-08 01:07:17 -0700

    Added reset in console log

--- js-data-structures/src/concepts/playground/utilities.js
+++ js-data-structures/src/concepts/playground/utilities.js
@@ -9,20 +9,27 @@ const colors = {
   blueBright: [94, 39],
   magentaBright: [95, 39],
   cyanBright: [96, 39],
+  blue: [34, 89],
+  yellow: [33, 89],
+  red: [31, 89],
+  cyan: [36, 89],
+  green: [32, 89],
 }
 
+const reset = "\x1b[0m";
+
 /**
- * add color to text, for example, \x1b[92m___\x1b[39m
+ * add color to text, for example, \x1b[92m___\x1b[39m + \x1b[0m
  * @param {[number, number]} color 
  * @param {string[]} strs 
  */
 function toLogString(color, strs) {
   // let text = strs.reduce((prev, current) => `${prev} ${current}`);
-  return `\x1b[${color[0]}m${strs.join(" ")}\x1b[${color[1]}m`;
+  return `\x1b[${color[0]}m${strs.join(" ")}\x1b[${color[1]}m${reset}`;
 }
 
 function toInfoLog(...strs) {
-  return toLogString(colors.greenBright, strs);
+  return toLogString(colors.green, strs);
 }
 
 function toWarningLog(...strs) {
--- js-data-structures/src/concepts/playground/utilities.ts
+++ js-data-structures/src/concepts/playground/utilities.ts
@@ -9,7 +9,14 @@ const colors: { [key: string]: [number, number] } = {
   blueBright: [94, 39],
   magentaBright: [95, 39],
   cyanBright: [96, 39],
+  blue: [34, 89],
+  yellow: [33, 89],
+  red: [31, 89],
+  cyan: [36, 89],
+  green: [32, 89],
 }
+
+const reset = "\x1b[0m";
 
 /**
  * add color to text, for example, \x1b[92m___\x1b[39m, 
@@ -18,7 +25,7 @@ const colors: { [key: string]: [number, number] } = {
  */
 function toLogString(color: [number, number]): (...strs: string[]) => string {
   return function (...strs) {
-    return `\x1b[${color[0]}m${strs.join(" ")}\x1b[${color[1]}m`;
+    return `\x1b[${color[0]}m${strs.join(" ")}\x1b[${color[1]}m${reset}`;
   }
 }
 
