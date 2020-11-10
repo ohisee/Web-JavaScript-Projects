abb0f11ad52363396eec71e2c7024e40cf62c445
ohisee <breakosin@hotmail.com>
2020-11-10 00:38:53 -0800

    Update undirected graph

--- js-data-structures/src/interview/graphs/UndirectedUnweightedGraph.js
+++ js-data-structures/src/interview/graphs/UndirectedUnweightedGraph.js
@@ -66,6 +66,111 @@ class Graph {
   }
 
   /**
+   * breadth first search on this graph, explore node's all child nodes first 
+   * using a queue to hold visited nodes while traversing 
+   * @param {string} startVertex 
+   */
+  breadthFirstSearchIterative(startVertex) {
+    /** @type {string[]} */
+    const queue = [startVertex];
+    /** @type {Object<string, boolean>} */
+    const visitedNodes = {};
+    /** @type {string[]} */
+    const visited = [];
+    if (this.adjacentList[startVertex]) {
+      visitedNodes[startVertex] = true;
+      while (queue.length > 0) {
+        let vertex = queue.shift();
+        let neighbors = this.adjacentList[vertex];
+        if (neighbors) {
+          visited.push(vertex);
+          for (let neighbor of neighbors) {
+            if (!visitedNodes[neighbor]) {
+              visitedNodes[neighbor] = true;
+              queue.push(neighbor);
+            }
+          }
+        }
+      }
+    }
+    return visited;
+  }
+
+  /**
+   * breadth first search on this graph, explore node's all child nodes first 
+   * using a queue to hold visited nodes while traversing 
+   * @param {string} startVertex 
+   * @param {string[]} queue 
+   * @param {string[]} visited 
+   * @param {Object<string, boolean>} visitedNodes 
+   */
+  breadthFirstSearchRecursive(startVertex, queue = [startVertex], visited = [], visitedNodes = {}) {
+    if (queue.length === 0) {
+      return visited;
+    }
+    let vertex = queue.shift();
+    let neighbors = this.adjacentList[vertex];
+    if (neighbors) {
+      visitedNodes[vertex] = true;
+      visited.push(vertex);
+      for (let neighbor of neighbors) {
+        if (!visitedNodes[neighbor]) {
+          visitedNodes[neighbor] = true;
+          queue.push(neighbor);
+        }
+      }
+    }
+    return this.breadthFirstSearchRecursive(startVertex, queue, visited, visitedNodes);
+  }
+
+  /**
+   * explore as far as it can down one branch before back tracking 
+   * using a stack to hold visited nodes while traversing  
+   * @param {string} startVertex 
+   */
+  depthFirstSearchIterative(startVertex) {
+    /** @type {string[]} */
+    const stack = [startVertex];
+    /** @type {Object<string, boolean>} */
+    const visitedNodes = {};
+    /** @type {string[]} */
+    const visited = [];
+    if (this.adjacentList[startVertex]) {
+      while (stack.length > 0) {
+        let vertex = stack.pop();
+        if (vertex && !visitedNodes[vertex]) {
+          visitedNodes[vertex] = true; // mark this vertex as visited
+          visited.push(vertex); // store this vertex 
+          stack.push(...this.adjacentList[vertex]); // track all edges in stack 
+        }
+      }
+    }
+    return visited;
+  }
+
+  /**
+   * explore as far as it can down one branch before back tracking 
+   * recursively call this function to visit every vertex of a node 
+   * @param {string} startVertex 
+   * @param {string[]} visited 
+   * @param {Object<string, boolean>} visitedNodes 
+   */
+  depthFirstSearchRecursive(startVertex, visited = [], visitedNodes = {}) {
+    let neighbors = this.adjacentList[startVertex];
+    if (!neighbors) {
+      return visited;
+    }
+    visited.push(startVertex);
+    visitedNodes[startVertex] = true;
+    for (let vertex of neighbors) {
+      if (!visitedNodes[vertex]) {
+        this.depthFirstSearchRecursive(vertex, visited, visitedNodes);
+      }
+    }
+    return visited;
+  }
+
+  /**
    * @returns {string[]} vertices of this graph 
    */
   getAllVertices() {
