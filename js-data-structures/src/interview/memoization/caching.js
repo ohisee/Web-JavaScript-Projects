90b3483a93b5973e8354d681e31e196dccfe7148
ohisee <breakosin@hotmail.com>
2020-11-13 00:42:46 -0800

    Update memoization caching

--- js-data-structures/src/interview/memoization/caching.js
+++ js-data-structures/src/interview/memoization/caching.js
@@ -105,3 +105,104 @@ console.log("Max gain [2, 7, 9, 3, 1] is", rob2(gains), "--- should print 12");
 
 gains = [6, 100, 1, 7, 8];
 console.log("Max gain [6, 100, 1, 7, 8] is", rob2(gains), "--- should print 108");
+
+/**
+ * find max difference of two number that is greater zero 
+ * @param {number[]} prices 
+ * @returns {number} max profit 
+ */
+function maxProfit(prices) {
+  if (prices.length <= 1) {
+    return 0;
+  }
+  let max = 0;
+  let minPrice = prices[0];
+  // start at 2nd number at index 1  
+  for (let i = 1; i < prices.length; i++) {
+    max = Math.max(prices[i] - minPrice, max);
+    minPrice = Math.min(prices[i], minPrice);
+  }
+  return max;
+}
+
+let prices = [7, 1, 5, 3, 6, 4];
+console.log("Max profit is", maxProfit(prices));
+
+prices = [7, 6, 4, 3, 2, 1];
+console.log("Max profit is", maxProfit(prices));
+
+prices = [7, 1];
+console.log("Max profit is", maxProfit(prices));
+
+prices = [7, 10];
+console.log("Max profit is", maxProfit(prices));
+
+prices = [7, 1, 10, 11, 12, 13, 14, 15, 16];
+console.log("Max profit is", maxProfit(prices));
+
+/**
+ * either climb 1 or 2 steps, find number of distinct ways to climb to 
+ * the top n 
+ * @param {number} n non-zero, non-negative 
+ * @returns {number} number of distinct ways  
+ */
+function climbStairs(n) {
+  let cache = [1];
+  for (let i = 1; i <= n; i++) {
+    if (i === 1) {
+      cache[i] = cache[i - 1];
+    } else {
+      cache[i] = cache[i - 1] + cache[i - 2]
+    }
+  }
+  return cache.pop();
+}
+
+let steps = 2;
+console.log("Ways to climb stairs", climbStairs(steps));
+
+steps = 3;
+console.log("Ways to climb stairs", climbStairs(steps));
+
+steps = 4;
+console.log("Ways to climb stairs", climbStairs(steps));
+
+steps = 5;
+console.log("Ways to climb stairs", climbStairs(steps));
+
+steps = 7;
+console.log("Ways to climb stairs", climbStairs(steps));
+
+/**
+ * a different approach, using variables to store steps 
+ * @param {number} n non-zero, non-negative 
+ */
+function climbStairs2(n) {
+  let prevStep1 = 1;
+  let prevStep2 = 0;
+  let total = 0;
+  for (let i = 1; i <= n; i++) {
+    total = prevStep1 + prevStep2;
+    prevStep2 = prevStep1;
+    prevStep1 = total;
+  }
+  return total;
+}
+
+steps = 1;
+console.log("Ways to climb stairs", climbStairs2(steps));
+
+steps = 2;
+console.log("Ways to climb stairs", climbStairs2(steps));
+
+steps = 3;
+console.log("Ways to climb stairs", climbStairs2(steps));
+
+steps = 4;
+console.log("Ways to climb stairs", climbStairs2(steps));
+
+steps = 5;
+console.log("Ways to climb stairs", climbStairs2(steps));
+
+steps = 7;
+console.log("Ways to climb stairs", climbStairs2(steps));
