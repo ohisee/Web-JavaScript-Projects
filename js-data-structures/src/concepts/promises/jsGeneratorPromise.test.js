15d2677eb471c70305df7e183b0025c4e8a16c91
ohisee <breakosin@hotmail.com>
2021-01-06 16:14:19 -0800

    Create unit test case for generator

---
+++ js-data-structures/src/concepts/promises/jsGeneratorPromise.test.js
@@ -0,0 +1,62 @@
+/**
+ * @fileoverview generator unit test 
+ */
+const { asyncGeneratorHandler } = require("./jsGeneratorPromise");
+
+describe("Generator unit test", () => {
+
+  test("should be defined", () => {
+    expect(asyncGeneratorHandler).toBeDefined();
+  });
+
+  test("should return a result", (done) => {
+    const gen = function* () {
+      yield Promise.resolve(2);
+    }
+    expect.assertions(1);
+    asyncGeneratorHandler(gen).call().then(result => {
+      expect(result).toEqual(2);
+      done();
+    });
+  });
+
+  test("should return a result", (done) => {
+    const gen = function* () {
+      let n1 = yield Promise.resolve(2);
+      let n2 = yield Promise.resolve(10);
+      yield n1 + n2;
+    }
+    expect.assertions(1);
+    asyncGeneratorHandler(gen).call().then(result => {
+      expect(result).toEqual(12);
+      done();
+    });
+  });
+
+  test("should return a result", (done) => {
+    const gen = function* (n3) {
+      let n1 = yield Promise.resolve(2);
+      let n2 = yield Promise.resolve(10);
+      yield n1 + n2 + n3;
+    }
+    expect.assertions(1);
+    asyncGeneratorHandler(gen).call(this, 20).then(result => {
+      expect(result).toEqual(32);
+      done();
+    });
+  });
+
+  test("should return a result", (done) => {
+    const gen = function* (str3) {
+      let str1 = yield Promise.resolve("hello there, ");
+      let str2 = yield Promise.resolve("how");
+      yield str1 + str2 + str3;
+    }
+    expect.assertions(1);
+    const generatorHandler = asyncGeneratorHandler(gen);
+    generatorHandler.call(generatorHandler, " are you?").then(result => {
+      expect(result).toEqual("hello there, how are you?");
+      done();
+    });
+  });
+});
