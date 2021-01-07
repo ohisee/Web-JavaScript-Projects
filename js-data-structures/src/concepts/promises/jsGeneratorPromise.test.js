adcce95cc97b544a751400de00edaaf03eefc237
ohisee <breakosin@hotmail.com>
2021-01-07 00:37:49 -0800

    Update to include test cases

--- js-data-structures/src/concepts/promises/jsGeneratorPromise.test.js
+++ js-data-structures/src/concepts/promises/jsGeneratorPromise.test.js
@@ -59,4 +59,30 @@ describe("Generator unit test", () => {
       done();
     });
   });
+
+  test("should return a result", (done) => {
+    const gen = function* (n3) {
+      let n1 = yield Promise.resolve(2);
+      let n2 = yield Promise.resolve(10);
+      yield n1 + n2 + n3;
+    }
+    expect.assertions(1);
+    return asyncGeneratorHandler(gen).call(this, 20).then(result => {
+      expect(result).toEqual(32);
+      done();
+    });
+  });
+
+  test("should return a result", () => {
+    const gen = function* (str3) {
+      let str1 = yield Promise.resolve("hello there, ");
+      let str2 = yield Promise.resolve("how");
+      yield str1 + str2 + str3;
+    }
+    expect.assertions(1);
+    const generatorHandler = asyncGeneratorHandler(gen);
+    return generatorHandler.call(generatorHandler, " are you?").then(result => {
+      expect(result).toEqual("hello there, how are you?");
+    });
+  });
 });
