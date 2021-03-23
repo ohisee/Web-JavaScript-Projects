9622ad8a259fe17804c5c094b0b12cff007dc94a
ohisee <breakosin@hotmail.com>
2021-03-22 20:41:30 -0700

    Update to include more routes

--- typescript-node-project/src/app.ts
+++ typescript-node-project/src/app.ts
@@ -2,15 +2,26 @@
  * @fileoverview App server
  */
 import express, { NextFunction } from "express";
-import { json } from "body-parser";
+// import { json } from "body-parser";
 import todoRoutes from "./routes/todos";
+import mainRoute from "./routes/main";
+import ignore from "./routes/icoroute";
+import path from "path";
 
 const app = express();
 
 // JSON middleware, incoming message must be in JSON format
-app.use(json());
+app.use(express.json());
+// form middleware 
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/todos', todoRoutes);
+app.use('/', mainRoute);
+app.use("/favicon.ico", (req, res) => {
+  res.writeHead(200, { "Conten-Type": "image/x-icon" }).end();
+});
+
+app.use(express.static(path.resolve(__dirname, "public")));
 
 app.use((
   err: Error,
@@ -20,4 +31,6 @@ app.use((
   res.status(500).json({ message: err.message });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("running at 3000")
+});
