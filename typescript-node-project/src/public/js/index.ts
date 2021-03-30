b4a70e624b2b355776e79085228b1d616477f005
ohisee <breakosin@hotmail.com>
2021-03-30 00:31:58 -0700

    Create an index js file

---
+++ typescript-node-project/src/public/js/index.ts
@@ -0,0 +1,10 @@
+/**
+ * @fileoverview index 
+ */
+
+(function() {
+  const emailEl = document.querySelector("#email") as HTMLInputElement;
+  const passwdEl = document.querySelector("#password") as HTMLInputElement;
+  emailEl.value = "";
+  passwdEl.value = "";
+})();
