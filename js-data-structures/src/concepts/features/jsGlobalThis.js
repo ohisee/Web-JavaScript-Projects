17c9ebed61b1c7d74b84bf89c8a87d458f1da348
ohisee <breakosin@hotmail.com>
2021-06-28 00:20:59 -0700

    Continued in JavaScript development

---
+++ js-data-structures/src/concepts/features/jsGlobalThis.js
@@ -0,0 +1,8 @@
+/**
+ * @fileoverview JS globalThis 
+ * 
+ * untify different platform outside of browsers to have access to globalThis object 
+ */
+
+console.log(global); // only in Node.js 
+console.log(globalThis); // also exists in brower i.e. Window object 
