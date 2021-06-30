c4cad46e146e826a7b8f8f77afeed0c7ea760723
ohisee <breakosin@hotmail.com>
2021-06-30 00:11:31 -0700

    Continued in JavaScript development

--- js-data-structures/src/todolist/index.js
+++ js-data-structures/src/todolist/index.js
@@ -7,8 +7,12 @@
   const todoItemListEl = document.querySelector(".todoitem_list");
   /** @type {NodeListOf<HTMLDivElement>} */
   const els = document.querySelectorAll(".todoitem");
+  /** @type {NodeListOf<HTMLDivElement>} */
+  const spaceEls = document.querySelectorAll(".spaceBetween");
   /** @type {string} */
   const bgClass = "dragOverBackground";
+  const moveUpClass = "moveUp";
+  const moveDownClass = "moveDown";
 
   for (let i = 0; i < els.length; i++) {
     let el = els[i];
@@ -36,11 +40,31 @@
       let source = JSON.parse(data);
       /** @typedef {HTMLDivElement} */
       let sourceEl = document.getElementById(source.id);
-      if (sourceEl) {
+      if (sourceEl && i !== source.index) {
         render(i, source.index);
       }
       el.classList.remove(bgClass);
       event.preventDefault();
+    });
+  }
+
+  for (let j = 0; j < spaceEls.length; j++) {
+    let spaceEl = spaceEls[j];
+
+    spaceEl.addEventListener('dragenter', function (event) {
+      event.preventDefault();
+      let x = document.getElementById(spaceEl.dataset.x);
+      let y = document.getElementById(spaceEl.dataset.y);
+      x.classList.add(moveUpClass);
+      y.classList.add(moveDownClass);
+    });
+
+    spaceEl.addEventListener('dragleave', function (event) {
+      event.preventDefault();
+      let x = document.getElementById(spaceEl.dataset.x);
+      let y = document.getElementById(spaceEl.dataset.y);
+      x.classList.remove(moveUpClass);
+      y.classList.remove(moveDownClass);
     });
   }
 
